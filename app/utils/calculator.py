d59b3ce55ee25bcc2d265cf5e8022c4e6d57cac4
Adhithya Sivanesh <adhithyashivan@users.noreply.github.com>
2025-05-25 11:00:08 -0400

    Initial commit

---
+++ app/utils/calculator.py
@@ -0,0 +1,6 @@
+"""
+A utility module for basic arithmetic operations.
+This module provides functions to add and subtract numbers.
+"""
+def add(a: int, b: int) -> int: /* ... */
+def subtract(a: int, b: int) -> int: /* ... */
